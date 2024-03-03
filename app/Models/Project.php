2360a6e9382939851054ffadc487e3c3be45eeff
moonymel <musumeci.me@gmail.com>
2024-03-03 19:12:57 +0100

    [UPDATE] created relation many to many projects/technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Type;
+use App\Models\Technology;
 
 class Project extends Model
 {
@@ -13,6 +14,10 @@ class Project extends Model
 
     public function type() {
         return $this->belongsTo(Type::class); 
+    }
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
     }
         
 }
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
 
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable = ['name'];
+
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
 }
